9b3aacdaf5385084b1dd9ae1a463d365f7ad7312
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-01-04 00:08:50 +0600

    end the navbar section

--- js/script.js
+++ js/script.js
@@ -2,4 +2,41 @@ let searchForm = document.querySelector(".search-form");
 
 document.querySelector("#search-btn").onclick = () => {
   searchForm.classList.toggle("active");
+  shoppingCart.classList.remove("active");
+  logingForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = () => {
+  shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+  logingForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let logingForm = document.querySelector(".loging-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  logingForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  logingForm.classList.remove("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+};
+
+window.onscroll = () => {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  logingForm.classList.remove("active");
+  navbar.classList.remove("active");
 };
