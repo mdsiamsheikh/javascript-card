1052ac98d2968eec32e06a83cb4d25ca02776507
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-01-11 17:40:51 +0600

    add a riviw section

--- js/script.js
+++ js/script.js
@@ -60,3 +60,23 @@ var swiper = new Swiper(".product-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
